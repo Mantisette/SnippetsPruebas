a2e8ca978fb48e2471a1208ca06f82149a0fee0f
Cleo Sánchez <83270683+Mantisette@users.noreply.github.com>
2023-05-11 13:48:39 +0200

    Añadir enunciados a los puzles

--- codingame-puzzles/descending-loops.c
+++ codingame-puzzles/descending-loops.c
@@ -1,17 +1,21 @@
 // descending-loops.c
 
+/**
+ *   The Descent
+ * At the start of each game turn, you are given the height of the 8 mountains
+ * from left to right. By the end of the game turn, you must fire on the highest
+ * mountain by outputting its index (from 0 to 7).
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game where you are given inputs
+ * (the heights of the mountains) and where you have to print an output (the
+ * index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+**/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-
-/**
-* The while loop represents the game.
-* Each iteration represents a turn of the game
-* where you are given inputs (the heights of the mountains)
-* and where you have to print an output (the index of the mountain to fire on)
-* The inputs you are given are automatically updated according to your last actions.
-**/
 
 int main()
 {
