222b842d5506bbd5ce50e811731435ca9b0565ee
Cleo Sánchez <83270683+Mantisette@users.noreply.github.com>
2023-05-18 10:16:51 +0200

    Bugfix de condiciones
    No me enorgullezco mucho de este porque quiero mostrar el numero original pero me da pereza hacerle una copia solo para eso entonces pues bueno.

--- c/bloque_1-fundamentos/13-factoriales.c
+++ c/bloque_1-fundamentos/13-factoriales.c
@@ -3,25 +3,22 @@
 // positivo. Nota: factorial de un número n es n!=n*(n-1)*(n-2)*...*1.
 // Ejemplo: 6!=6*5*4*3*2*1=720
 
-// TODO: esto tambien esta roto? a lo mejor el compilador de replit se ha hecho
-// chapa, o a lo mejor la funcion scanf esta dando problemas. hay que revisar.
-
 #include <stdio.h>
 
 int main()
 {
-  int numero = 0;
+  int numero = 1;
   int resultado = 1;
 
   printf("Introduzca un número >");
   scanf("%d", &numero);
 
-  while (numero >= 0)
+  while (numero >= 1)
   {
     resultado = resultado * numero;
     numero--;
   }
 
-  printf("El factorial de %d es %d\n", numero, resultado);
+  printf("El factorial es: %d\n", resultado);
   return 0;
 }
