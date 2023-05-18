d970000b725fda23e671dcbb754acc1418d60621
Cleo Sánchez <83270683+Mantisette@users.noreply.github.com>
2023-05-18 10:11:39 +0200

    Bugfix de condiciones

--- c/bloque_1-fundamentos/12-contador-cifras.c
+++ c/bloque_1-fundamentos/12-contador-cifras.c
@@ -1,8 +1,6 @@
 // EXTRA 7. Pide (y exige que sea así) un número entero estrictamente positivo.
 // El programa mostrará cuántas cifras tiene (ej: 3120 → 4).
 
-// TODO: no se qué le pasa pero esto está roto
-
 #include <stdio.h>
 
 int main()
@@ -12,17 +10,15 @@ int main()
   printf("Introduzca un número positivo >");
   scanf("%d", &numero);
 
-  printf("%d\n", numero);
-
-  if (numero < 0)
+  if (numero <= 0)
   {
     printf("El número introducido no es positivo.\n");
     return 0;
   }
-
-  while (numero >= 0)
+  
+  while (numero >= 1)
   {
-    cifras ++;
+    cifras++;
     numero = numero / 10;
   }
 
